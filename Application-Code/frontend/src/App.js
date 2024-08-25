3502f3f6033afeff1b8d98159f871894ff6d0e4b
sujalsharmaa <sujalsharma151@gmail.com>
2024-08-25 18:47:40 +0530

    change 2

--- Application-Code/frontend/src/App.js
+++ Application-Code/frontend/src/App.js
@@ -11,7 +11,7 @@ class App extends Tasks {
         return (
             <div className="app">
                 <header className="app-header">
-                    <h1>My To-Do List- change1</h1>
+                    <h1>My To-Do List- change1 change 2</h1>
                 </header>
                 <div className="main-content">
                     <Paper elevation={3} className="todo-container">
